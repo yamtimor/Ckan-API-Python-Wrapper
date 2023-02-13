d3209f3837047a81cf4e723a0a1a2a4b7b191c67
yamtimor <yamtimor@gmail.com>
2023-02-13 20:05:29 +0200

    adding the setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import codecs
 import os
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Ckan-API-Python-Wrapper'
@@ -8,10 +16,18 @@ LONG_DESCRIPTION = 'Ckan-API-Python-Wrapper'
 
 # Setting up
 setup(
-    name="Ckan-API-Python-Wrapper",
-    version=VERSION,
-    author="Yam Timor",
-    description=DESCRIPTION,
-    packages=find_packages(),
-    install_requires = ['requests', 'json', 'pandas']
+    name = 'Ckan-API-Python-Wrapper',
+    version = VERSION,
+    author = 'Yam Timor',
+    description = DESCRIPTION,
+    long_description = long_description,
+    packages=['Ckan-API-Python-Wrapper'],
+    package_dir={'': 'src'}
+    install_requires = ['requests', 'json', 'pandas'],
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 1 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
 )
