8f5214ed2b1e2c0b5c93f9d1b2ab5813cf9ead18
yamtimor <yamtimor@gmail.com>
2023-02-14 13:08:22 +0200

    make the code more clear

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 VERSION = '0.0.1'
 DESCRIPTION = 'Ckan-API-Python-Wrapper'
 LONG_DESCRIPTION = 'Ckan-API-Python-Wrapper'
+
 
 # Setting up
 setup(
